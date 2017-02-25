f6dee63d0655b16c60a52dce43c2c3677b14c28e
Jacinto Martínez Duarte <jasintomd@hotmail.com>
2017-02-25 19:58:01 +0100

    Mejorado

--- public/script.js
+++ public/script.js
@@ -1,15 +1,182 @@
+    function escondeLogin(){
+        $(".login").hide();
+        $("#todo").addClass("listo");
+        $("#cabecera").addClass("listo");
+        $("#cabecera").show();
+        $("#barraUsuarios").show();
+        $(".container").show();
+        $("#barraRooms").show();
+    }
+
+    function cambiaListas(destino){
+        $("#salaActual").html(destino);
+        $(".mensajes ul.activo").addClass("escondido").removeClass("activo");
+        $("#listaMensajes"+destino).addClass("activo").removeClass("escondido")
+    }
+    function validaNick(nick){
+        var reg = /\s/g;
+        if (nick.match(reg) == null && 0 < nick.length && nick.length < 16){
+            return true
+        } else {
+            $("#condiciones").show();
+            return false
+        }
+    }
+
 
 $(document).ready(function(){
     var socket = io();
+    var miNick = "";
+    var intervalo = null;
+    var listaSalasCliente = []
     
     
+    
+    $("#btnLogin").click( function (){
+        nickUsuario = $("#nickUsuario").val();   
+        if(validaNick(nickUsuario)){
+            $("#superior").html(nickUsuario);
+            $("#salaActual").html("Principal");
+            imgUsuario = $("#imgSeleccionada").attr('src');
+            $("#miAvatar").attr("src", imgUsuario)
+            estadoUsuario = $("#estadoUsuario").val();
+            
+            if(estadoUsuario == ""){
+                estadoUsuario = "Disponible"
+            }
+                
+            socket.emit("cogeNick", nickUsuario, imgUsuario, estadoUsuario);
+            escondeLogin();       
+            
+                
+           }
+        
+                
+        });
+       
+    
+    function chequea(){
+        socket.emit("dejaEscribir", nickUsuario)
+        intervalo = null;
+    }
+    
+    $("#sitioQueEscribe").keydown(function (event){
+        if(!intervalo){
+            socket.emit("escribiendo", nickUsuario)
+            
+        intervalo = setTimeout(chequea,1000);     
+        } else {
+            
+            clearTimeout(intervalo);
+            intervalo = setTimeout(chequea,1000);
+        }
+                
+        
+        if(event.keyCode == 13){            
+            
+            socket.emit("nuevoMensaje", $("#sitioQueEscribe").val(), $("#salaActual").html() );                 
+            $("#sitioQueEscribe").val("");
+            return false;
+        }
+    })
+    
+    socket.on("escribiendo", function(nickUsuario){    
+        $("#"+nickUsuario+" #escribiendo").show();
+        
+    })
+    socket.on("dejaEscribir", function(nickUsuario){
+        $("#"+nickUsuario+" #escribiendo").hide();
+        
+    })
+        
+    
+    $("#selImg").click( function (){
+        $(".contieneImagenes").show()
+        $(".imgPerfil").click(function(){
+            $("#imgSeleccionada").attr("src", this.src);
+            $(".contieneImagenes").hide()
+        })
+    })
+    socket.on("creaRoomPrivada", function(sala, usuario){
+        
+        if(listaSalasCliente.indexOf(sala) == -1){
+            if(sala != nickUsuario){
+                listaSalasCliente.push(sala);
+                $(".mensajes").append($("<ul class='privado escondido' id='listaMensajes"+sala+"'></ul>"));
+                cambiaListas(sala)
+                
+            } else{
+                listaSalasCliente.push(usuario);
+                $(".mensajes").append($("<ul class='privado escondido' id='listaMensajes"+usuario+"'></ul>"));
+                cambiaListas(usuario)
+                
+            }
+        } 
+    })
+    
+   socket.on("cargaRooms", function(listaRooms){
+        $("#listaSalas").html("");
+        var l = listaRooms.concat(listaSalasCliente);
+        for(let i=0; i<l.length; i++){
+            $("#listaSalas").append($("<li class='sala'>").text(l[i]).click(function(){
+               cambiaListas(this.innerHTML)
+            }))
+       }
+   })
+    
+   
     $("#chat").submit(function(){
         socket.emit("nuevoMensaje", $("#sitioQueEscribe").val());
         $("#sitioQueEscribe").val("");
         return false;
     });
-    socket.on("nuevoMensaje", function(mensaje){
-        $("#listaMensajes").append($("<li>").text(mensaje));
+    
+    
+    socket.on("nuevoMensaje", function(mensaje, lista){
+        console.log(mensaje);
+        if($("#listaMensajes"+lista).length != 0){
+            var sitio = "#listaMensajes"+lista;
+        }else{
+            var sitio = "#listaMensajes"+mensaje.split(":")[0];
+        }
+        if($(sitio).hasClass("privado") && (!mensaje.startsWith(nickUsuario+": ") && !mensaje.startsWith(listaSalasCliente))){
+            mensaje=null;
+            
+        }
+        //SI SE PASA DE 70 CARACTERES HAY QUE HACER UN \n POR ALGUN SITIO
+        if(mensaje != nickUsuario+": " && !mensaje.startsWith("undefined")){
+            if(mensaje.startsWith(nickUsuario+": ")){
+                $(sitio).append($("<li class='miMensaje'>").text(mensaje).append($("<img class='imgMiBocadillo' src='images/coma2.png'>")));
+            }else{
+                
+                $(sitio).append($("<img class='imgBocadillo' src='images/coma1.png'>"));
+                $(sitio).append($("<li>").text(mensaje));
+            }       
+        }
     });
+    
+    
+    socket.on("asignaNick", function(nick){
+        miNick = nick;
+    })
+    
+    
+    socket.on("nuevoUsuario", function(numeroUsuarios){
+        $("#numUsuarios").html(numeroUsuarios);
+    })
+    
+    
+    socket.on("nuevaListaUsuarios", function(listaUsuarios){
+        $("#listaUsuarios").html("");
+        for(let i=0; i<listaUsuarios.length; i++){
+            $("#listaUsuarios").append($("<li id='"+listaUsuarios[i].usuario+"'>").text(listaUsuarios[i].usuario).append($("<img src='" + listaUsuarios[i].img + "'>")).append("<img class='escondido' id='escribiendo' src=images/escribiendo.gif>").append("<br/><span>"+listaUsuarios[i].estado+"</span>").click(function(){
+                if(listaUsuarios[i].usuario != nickUsuario){                    
+                    socket.emit("creaRoomPrivada", listaUsuarios[i].usuario);
+                }
+                }).append($("<hr>")));
+        }
+    })
+    
+    
     
 })
--- server/server.js
+++ server/server.js
@@ -2,18 +2,83 @@ var express = require("express")
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var numeroUsuarios = 0;
+var listaUsuarios = [];
+var listaRooms = ["Principal", "América", "África", "Asia"]
 
 app.use(express.static("public"));
 
-http.listen( 3128, function(){
+http.listen( process.env.PORT || 3128, function(){
     console.log("Está escuchando en el puerto que tiene que escuchar")
 })
 
+function anadeLista(usuario, img, estado){
+    listaUsuarios.push({usuario, img, estado});
+    console.log(listaUsuarios)
+}
+
+function quitaLista(usuario){
+    listaUsuarios.splice(listaUsuarios.indexOf(usuario),1);
+    
+}
 
 
-io.on("connection", function(socket){
-    io.emit("nuevoMensaje", "Alguien ha entrado en el chat");
-    socket.on("nuevoMensaje", function(mensaje){
-        io.emit("nuevoMensaje", mensaje);
+io.on("connection", function(socket){  
+    
+    socket.on("cogeNick", function(nickUsuario, imgUsuario, estadoUsuario){
+        
+            socket.nick = nickUsuario;
+            socket.img = imgUsuario;
+            socket.estado = estadoUsuario;
+            socket.room = "Principal";
+            anadeLista(socket.nick, socket.img, socket.estado);
+            socket.join("Principal");
+            socket.join(nickUsuario);
+            io.emit("cargaRooms", listaRooms);
+            io.to("Principal").emit("nuevoMensaje", socket.nick +" ha entrado en el chat", "Principal");
+            io.emit("nuevaListaUsuarios", listaUsuarios);
+            io.emit("asignaNick", socket.id);
+            numeroUsuarios++;
+            io.emit("nuevoUsuario", numeroUsuarios);
+        
     })
+    
+    socket.on("repeNick", function(nickUsuario){
+        if(listaUsuarios.indexOf(nickUsuario) == -1){
+            socket.emit("nickValido", nickUsuario);
+        }
+    })
+   
+    socket.on("nuevoMensaje", function(mensaje, lista){
+        io.emit("nuevoMensaje", socket.nick +": "+ mensaje, lista);
+
+    })
+    
+    socket.on("escribiendo", function(nickUsuario){
+        io.emit("escribiendo", nickUsuario)
+        
+    })
+    socket.on("dejaEscribir", function(nickUsuario){
+        io.emit("dejaEscribir", nickUsuario)
+        
+    })
+    
+    socket.on("creaRoomPrivada", function(sala){
+            console.log(sala, socket.nick);
+            socket.join(sala);
+            io.sockets.in(sala).emit("creaRoomPrivada", sala, socket.nick)
+            io.sockets.in(sala).emit("nuevoMensaje", socket.nick+" ha abierto un chat privado", socket.nick)
+         
+        io.emit("cargaRooms", listaRooms);
+    })
+    
+    socket.on('disconnect', function () {
+        quitaLista(socket.nick);
+        numeroUsuarios = listaUsuarios.length
+        
+        io.emit("nuevaListaUsuarios", listaUsuarios);
+        io.emit("nuevoUsuario", numeroUsuarios);
+        io.emit("nuevoMensaje", socket.nick + " se ha desconectado")
+});
+    
 })
